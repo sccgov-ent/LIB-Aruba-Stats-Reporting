71f97c248f1efcc1effb7d6eeb472a40b1b3700b
ian-may <ian.may@lib.sccgov.org>
2026-02-02 15:21:11 -0800

    Update setup.sh

--- package/app/setup.sh
+++ package/app/setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 sudo dnf update
-sudo dnf groupinstall "Development Tools"
+sudo dnf install gcc
+sudo dnf install python3-devel
 sudo dnf install python3.12 -y
 sudo dnf install python3.12-pip -y
 curl -LsSO https://r.mariadb.com/downloads/mariadb_repo_setup
 chmod +x mariadb_repo_setup
 sudo ./mariadb_repo_setup \ --mariadb-server-version="mariadb-10.6"
 rm mariadb_repo_setup
-sudo dnf install MariaDB-shared MariaDB-devel
+sudo dnf install MariaDB-shared MariaDB-devel libmariadb-dev
 python -m pip install --upgrade pip
 python -m venv venv
 source ./venv/bin/activate
