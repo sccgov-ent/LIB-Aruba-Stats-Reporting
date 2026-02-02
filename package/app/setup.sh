c4b6e6753f77217bf60e69c8e2f278c87f29effd
ian-may <ian.may@lib.sccgov.org>
2026-02-02 14:32:10 -0800

    Update setup.sh
    fixing for RHEL

--- package/app/setup.sh
+++ package/app/setup.sh
@@ -3,8 +3,13 @@
 sudo dnf update
 sudo dnf install python3.12 -y
 sudo dnf install python3.12-pip -y
-python -m venv
-./venv/Scripts/activate
+curl -LsSO https://r.mariadb.com/downloads/mariadb_repo_setup
+chmod +x mariadb_repo_setup
+sudo ./mariadb_repo_setup \ --mariadb-server-version="mariadb-10.6"
+rm mariadb_repo_setup
+sudo dnf install MariaDB-shared MariaDB-devel
+python -m venv venv
+source ./venv/bin/activate
 pip install dotenv
 pip install mariadb
 pip install requests
